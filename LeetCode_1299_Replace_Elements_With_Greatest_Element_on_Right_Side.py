e972f6b0b255398b03461ff94986374cb6f2eaa7
ronnitburman <ronnitburman@gmail.com>
2022-11-04 23:31:01 -0400

    Update LeetCode_1299_Replace_Elements_With_Greatest_Element_on_Right_Side.py

--- LeetCode_1299_Replace_Elements_With_Greatest_Element_on_Right_Side.py
+++ LeetCode_1299_Replace_Elements_With_Greatest_Element_on_Right_Side.py
@@ -4,10 +4,20 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
+        # if len(arr)>1:
+        #     for i in range(len(arr)-1):
+        #         arr[i]=max(arr[(i+1):])
+        #     arr[len(arr)-1]= -1
+        #     return arr
+        # else:
+        #     return[-1]
+
         if len(arr)>1:
-            for i in range(len(arr)-1):
-                arr[i]=max(arr[(i+1):])
-            arr[len(arr)-1]= -1
+            maxm = -1
+            for i in range(len(arr)):
+                temp = arr[len(arr)-i-1]
+                arr[len(arr)-i-1] = maxm
+                maxm = max(maxm, temp)
             return arr
         else:
-            return[-1]
+            return [-1]
