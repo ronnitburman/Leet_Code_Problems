16633c803535dc7e7b153a7aa92f61c317f9834a
ronnitburman <ronnitburman@gmail.com>
2022-11-13 20:51:43 -0500

    Update LeetCode_118_Pascals_Triangle.py

--- LeetCode_118_Pascals_Triangle.py
+++ LeetCode_118_Pascals_Triangle.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         res=[[1]]
         for i in range(1,numRows):
-            res.append([0,*res[i-1]])
+            res.append([0]+res[i-1])
             temp = res[i]+[0]
             for j in range(len(temp)-1):
                 res[i][j]=temp[j]+temp[j+1]
