54c072b53930f741faa01c73d9e115ff913a163a
ronnitburman <ronnitburman@gmail.com>
2022-11-14 22:16:34 -0500

    Update LeetCode_9_Palindrome_Number.py

--- LeetCode_9_Palindrome_Number.py
+++ LeetCode_9_Palindrome_Number.py
@@ -9,3 +9,16 @@ class Solution(object):
             if s[i]!=s[len(s)-i-1]:
                 return False
         return True
+        #in case we have to solve without using a string
+        # if x<0: 
+        #     return False
+        # div=1
+        
+        # while x>=10*div :
+        #     div*=10
+        
+        # while x:
+        #     if x//div != x%10: return False
+        #     x = (x%div)//10
+        #     div/=100
+        # return True
